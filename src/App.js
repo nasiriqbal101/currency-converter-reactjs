575badd38e8c1472e80d3e8697b1ba1bc461f6db
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-06-15 23:48:09 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = "https://api.exchangeratesapi.io/latest"
+
 function App() {
+  
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    
+  }, [])
+
   return (
     <>
       <h1>Convert</h1>
